4ad8e09ff07bd4179741f70bacc8bb7dd63c2231
Shawn <so>
2011-01-26 14:27:11 -0800

    s/apt-get/aptitude/

--- python/gae-flask/ubuntu-python2.5.sh
+++ python/gae-flask/ubuntu-python2.5.sh
@@ -3,15 +3,14 @@
 echo adding deadsnakes repository for Python 2.5 for Ubuntu 10.10
 
 sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update
-sudo apt-get install python2.5
+sudo aptitude update
+sudo aptitude install python2.5
 
-sudo apt-get install python2.5-dev
-sudo apt-get install libjpeg62-dev
-sudo easy_install-2.5 pil
+sudo aptitude install python2.5-dev
+sudo aptitude install libjpeg62-dev
 
-
-# apt-get install libssl-dev libbluetooth-dev
+# shouldn't be necessary
+# aptitude install libssl-dev libbluetooth-dev
 # wget http://pypi.python.org/packages/source/s/ssl/ssl-1.15.tar.gz
 # tar xzf ssl-1.15.tar.gz
 # cd ssl-1.15/
